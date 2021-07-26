d6147a244305b8ba8dbf1d652a4629f51eccc86b
sermpezis <sermpezis.pavlos@gmail.com>
2021-07-26 19:54:58 +0300

    updated select monitors analysis scripts

---
+++ TEMP_pavlos/plot_monitor_selections.py
@@ -0,0 +1,72 @@
+import json
+from matplotlib import pyplot as plt
+
+INPUT_FILENAME = 'select_monitors_1000.json'
+
+
+'''
+Load data
+'''
+with open(INPUT_FILENAME, 'r') as f:
+    DATA = json.load(f)
+    #  DATA.keys()  = 'asns naive', 'dist naive', 'asns greedy', 'dist greedy'
+
+
+
+'''
+Define ploting options
+'''
+linewidth = 3.0
+markersize = 10.0
+fontsize = 20
+common_plot_kwargs = {'linewidth':linewidth, 'markersize':markersize, 'markeredgewidth':linewidth}#, 'markerfacecolor':'None'
+
+
+'''
+Plots
+'''
+
+
+def plot_nb_monitors_vs_dist(DATA, MAX_X):
+    x_vector = list(range(0,MAX_X+1))
+    y_vector_naive = [DATA['dist naive'][i] / DATA['dist naive'][0] for i in range(MAX_X+1)]
+    y_vector_greedy = [DATA['dist greedy'][i] / DATA['dist greedy'][0] for i in range(MAX_X+1)]
+    plt.plot(x_vector, y_vector_naive, '--k', x_vector, y_vector_greedy, '-k', **common_plot_kwargs)
+    plt.legend(['Sorted', 'Greedy'], fontsize=fontsize)
+    plt.xlabel('# new monitors',fontsize=fontsize)
+    plt.ylabel('distance (normalized)',fontsize=fontsize)
+    plt.title('total distance = {}'.format(DATA['dist naive'][0]))
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    plt.grid(True)  
+    plt.tight_layout()
+    # plt.subplots_adjust(left=0.17, bottom=0.17)
+    plt.savefig('fig_set_monitors_vs_distance_{}.png'.format(MAX_X))
+    plt.close()
+
+plot_nb_monitors_vs_dist(DATA, 10)
+plot_nb_monitors_vs_dist(DATA, 100)
+plot_nb_monitors_vs_dist(DATA, 1000)
+
+
+
+def plot_monitors_vs_dist(DATA, MAX_X):
+    x_vector = list(range(1,MAX_X+1))
+    y_vector_naive = [DATA['dist naive'][i-1]-DATA['dist naive'][i] for i in range(1,MAX_X+1)]
+    y_vector_greedy = [DATA['dist greedy'][i-1]-DATA['dist greedy'][i] for i in range(1,MAX_X+1)]
+    plt.plot(x_vector, y_vector_naive, '--k', x_vector, y_vector_greedy, '-k', **common_plot_kwargs)
+    plt.legend(['Sorted', 'Greedy'], fontsize=fontsize)
+    plt.xlabel('i^{th} monitor',fontsize=fontsize)
+    plt.ylabel('improvement by i^{th} monitor',fontsize=fontsize)
+    # plt.title('total distance = {}'.format(DATA['dist naive'][0]))
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    plt.grid(True)  
+    plt.tight_layout()
+    # plt.subplots_adjust(left=0.17, bottom=0.17)
+    plt.savefig('fig_monitors_vs_distance_decrease_{}.png'.format(MAX_X))
+    plt.close()
+
+plot_monitors_vs_dist(DATA, 10)
+plot_monitors_vs_dist(DATA, 100)
+plot_monitors_vs_dist(DATA, 1000)
--- TEMP_pavlos/select_monitors.py
+++ TEMP_pavlos/select_monitors.py
@@ -2,6 +2,9 @@
 import json
 import gzip
 from matplotlib import pyplot as plt
+from collections import defaultdict
+
+INF = 1000000
 
 YYYY='2021'
 MM='07'
@@ -11,7 +14,7 @@ DIST_FILENAME = 'pfx.asn.min_distance{}.{}.{}.json'.format(YYYY,MM,DD)
 STATS_FILENAME = 'stats_no_country.{}.{}.{}.txt.gz'.format(YYYY,MM,DD)
 
 THRES = 1
-MAX_MONITORS = 1000
+MAX_MONITORS = 10000
 
 
 
@@ -60,18 +63,80 @@ for pfx in candidates.keys():
 
 
 # IPv4
-max_monitors = MAX_MONITORS
 sorted_asns = sorted(asn_score[4].keys(), key=asn_score[4].get)
+
+
+## naive
+total_dist = sum(pfx_mindist[4].values())
+total_dist_plot_naive = [total_dist]
+
+selected_asns_naive = sorted_asns[-MAX_MONITORS:]
+selected_asns_naive = selected_asns_naive[::-1]
+min_dist_naive = defaultdict()
+for pfx in candidates.keys():    
+        if (':' not in pfx):
+            min_dist_naive[pfx] = pfx_mindist[4][pfx]
+for i,next_asn in enumerate(selected_asns_naive):
+    print(i)
+    for pfx in candidates.keys():    
+        if (':' not in pfx):
+            # min_dist_naive[pfx]  = min([min_dist_naive[pfx], candidates[pfx].get(next_asn,INF)])
+            # improvement = max(0, pfx_mindist[4][pfx]- min_dist_naive[pfx])
+
+            improvement = max(0, min_dist_naive[pfx]-candidates[pfx].get(next_asn,INF))
+            min_dist_naive[pfx] -= improvement
+
+            # min_dist_naive = min([candidates[pfx][asn] for asn in selected_asns_naive[0:i+1] if asn in candidates[pfx].keys()]+[INF])
+            # improvement = max(0, pfx_mindist[4][pfx]- min_dist_naive)
+            total_dist -= improvement
+    total_dist_plot_naive.append(total_dist)
+
+print(selected_asns_naive)
+print(total_dist_plot_naive)
+
+## greedy
+# total_dist = sum(pfx_mindist[4].values())
+# total_dist_plot = [total_dist]
+# selected_asns = []
+# print('now \t\t\t\t\t total dist: {}'.format(total_dist))
+# while (len(sorted_asns) > 0) and (len(selected_asns) <MAX_MONITORS):
+#     best_asn = sorted_asns.pop()
+#     selected_asns.append(selected_asns)
+#     improvement = asn_score[4][best_asn]
+#     for pfx in candidates.keys():
+#         if (':' not in pfx) and (best_asn in candidates[pfx].keys()):
+#             if candidates[pfx][best_asn] < pfx_mindist[4][pfx]:
+#                 for other_asn in candidates[pfx].keys():
+#                     if other_asn != best_asn:
+#                         previous_improvement = max(0, pfx_mindist[4][pfx] - candidates[pfx][other_asn])
+#                         asn_score[4][ other_asn ] -= previous_improvement
+#                         asn_score[4][ other_asn ] += max(0, candidates[pfx][best_asn] - candidates[pfx][other_asn])
+#                 pfx_mindist[4][pfx] = candidates[pfx][best_asn]
+#             del candidates[pfx][best_asn]
+#     del asn_score[4][best_asn]
+#     sorted_asns = sorted(asn_score[4].keys(), key=asn_score[4].get)
+#     total_dist -= improvement
+#     total_dist_plot.append(total_dist)
+#     print('ASN: {}\t improvement: {}\t total dist: {}'.format(best_asn, improvement, total_dist))
+# print(total_dist_plot)
+
+
+
+## greedy v2
 total_dist = sum(pfx_mindist[4].values())
 total_dist_plot = [total_dist]
+selected_asns = []
+min_dist_greedy = defaultdict(lambda : INF)
 print('now \t\t\t\t\t total dist: {}'.format(total_dist))
-while (len(sorted_asns) > 0) and (max_monitors > 0):
+while (len(sorted_asns) > 0) and (len(selected_asns) <MAX_MONITORS):
     best_asn = sorted_asns.pop()
-    improvement = asn_score[4][best_asn]
-    max_monitors -= 1
+    selected_asns.append(best_asn)
+    # improvement = asn_score[4][best_asn]
     for pfx in candidates.keys():
         if (':' not in pfx) and (best_asn in candidates[pfx].keys()):
             if candidates[pfx][best_asn] < pfx_mindist[4][pfx]:
+                improvement = pfx_mindist[4][pfx] - candidates[pfx][best_asn]
+                total_dist -= improvement
                 for other_asn in candidates[pfx].keys():
                     if other_asn != best_asn:
                         previous_improvement = max(0, pfx_mindist[4][pfx] - candidates[pfx][other_asn])
@@ -81,9 +146,31 @@ while (len(sorted_asns) > 0) and (max_monitors > 0):
             del candidates[pfx][best_asn]
     del asn_score[4][best_asn]
     sorted_asns = sorted(asn_score[4].keys(), key=asn_score[4].get)
-    total_dist -= improvement
+    # total_dist -= improvement
     total_dist_plot.append(total_dist)
     print('ASN: {}\t improvement: {}\t total dist: {}'.format(best_asn, improvement, total_dist))
+print(total_dist_plot)
 
-plt.plot(range(MAX_MONITORS+1), total_dist_plot)
-plt.show()
+
+
+# plot results
+plt.plot(range(MAX_MONITORS+1), total_dist_plot, range(MAX_MONITORS+1),total_dist_plot_naive)
+plt.legend(['Greedy', 'Sorted'])
+plt.grid('on')
+plt.xlabel('# new monitors')
+plt.ylabel('total distance')
+plt.savefig('set_monitors_improvement_{}.png'.format(MAX_MONITORS))
+# plt.show()
+
+
+
+# D = defaultdict(list)
+# D['asns naive'] = selected_asns_naive
+# D['dist naive'] = total_dist_plot_naive
+# D['asns greedy'] = selected_asns
+# D['dist greedy'] = total_dist_plot
+D = {'asns naive': selected_asns_naive, 'dist naive': total_dist_plot_naive, 'asns greedy': selected_asns, 'dist greedy': total_dist_plot}
+print(selected_asns)
+print(D)
+with open('select_monitors_{}.json'.format(MAX_MONITORS), 'w') as f:
+    json.dump(D,f)
