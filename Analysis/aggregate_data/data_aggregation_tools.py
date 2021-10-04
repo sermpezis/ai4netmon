66ec21d228d8ac6c6c19ddf8369d05c080748542
xrhstosg <georgitsis1996@gmail.com>
2021-10-04 13:14:32 +0300

    Refactor code

--- Analysis/aggregate_data/data_aggregation_tools.py
+++ Analysis/aggregate_data/data_aggregation_tools.py
@@ -11,66 +11,79 @@ def keep_number(text):
     :return: 206924
     """
     num = re.findall(r'[0-9]+', text)
+
     return " ".join(num)
 
-def exists_in_perso(perso, asn):
+def create_df_from_AS_rank():
 
     """
-    :param perso: personal AS
-    :param asn: These ASn exist in AS_rank dataset
-    :return: True if an ASn from AS_rank dataset exists in personal dataset else returns False
+   Change the column names in order to know the features origin
+   :return: return the new dataframe
+   """
+    path = '../../Datasets/As-rank/asns.csv'
+    data = pd.read_csv(path, sep=",")
+    new_columns = ['AS_rank_' + str(i) for i in data.columns]
+    data = data.set_axis(new_columns, axis='columns', inplace=False)
+    data = data.set_index('AS_rank_asn')
+
+    return data
+
+def create_df_from_personal():
+
     """
-    return asn in perso.asn.values
+    :return: the a dataframe which contains only one column. This column has the ASn of personal dataset as integers
+    """
+    path = '../../Datasets/bgp.tools/perso.txt'
+    data = pd.read_csv(path, header=None)
+    # name the column
+    data.columns = ['asn']
+    # keep only the digits of the ASns
+    data['asn'] = data['asn'].apply(lambda x: keep_number(x))
+    data['personal_is_matched'] = 1
+    # needed to convert to a string first, then to an int.
+    data['asn'] = data['asn'].astype(str).astype(int)
+    data = data.set_index('asn')
+
+    return data
+
+def create_df_from_PeeringDB():
+
+    """
+    :return PeeringDB dataframe which contains only the features in the keep_keys list.
+    """
+    path = '../../Datasets/PeeringDB/peeringdb_2_dump_2021_07_01.json'
+    df = pd.read_json(path)
+    data = []
+    keep_keys = ['info_ratio', 'info_traffic', 'info_scope', 'info_type', 'info_prefixes4',
+                 'info_prefixes6', 'policy_general', 'ix_count', 'fac_count', 'created']
+    for row in df.net['data']:
+        net_row = []
+        for key in keep_keys:
+            if key in row:
+                net_row.append(row[key])
+            else:
+                net_row.append(None)
+        data.append(net_row)
+    df = pd.DataFrame(data, columns=keep_keys)
+    data = df
+
+    return data
 
 def create_df_from(dataset):
 
     if dataset =='AS_rank':
-        """
-        Change the column names in order to know the features origin
-        :return: return the new dataframe
-        """
-        data = pd.read_csv('../../Datasets/As-rank/asns.csv', sep=",")
-        new_columns = ['AS_rank_' + str(i) for i in data.columns]
-        data = data.set_axis(new_columns, axis='columns', inplace=False)
-        data = data.set_index('AS_rank_asn')
 
-        return data
+       data = create_df_from_AS_rank()
+       return data
+
     elif dataset == 'personal':
 
-        """
-        :return: the a dataframe which contains only one column. This column has the ASn of personal dataset as integers
-        """
-        data = pd.read_csv('../../Datasets/bgp.tools/perso.txt', header=None)
-        # name the column
-        data.columns = ['asn']
-        # keep only the digits of the ASns
-        data['asn'] = data['asn'].apply(lambda x: keep_number(x))
-        data['personal_is_matched'] = 1
-        # needed to convert to a string first, then to an int.
-        data['asn'] = data['asn'].astype(str).astype(int)
-        data = data.set_index('asn')
-
+        data = create_df_from_personal()
         return data
+
     elif dataset == 'PeeringDB':
 
-        """
-        :return PeeringDB dataframe which contains only the features in the keep_keys list.
-        """
-        df = pd.read_json('../../Datasets/PeeringDB/peeringdb_2_dump_2021_07_01.json')
-        data = []
-        keep_keys = ['info_ratio', 'info_traffic', 'info_scope', 'info_type', 'info_prefixes4',
-                     'info_prefixes6', 'policy_general', 'ix_count', 'fac_count', 'created']
-        for row in df.net['data']:
-            net_row = []
-            for key in keep_keys:
-                if key in row:
-                    net_row.append(row[key])
-                else:
-                    net_row.append(None)
-            data.append(net_row)
-        df = pd.DataFrame(data, columns=keep_keys)
-        data = df
-
+        data = create_df_from_PeeringDB()
         return data
 
 def dfs_concatanate(data_AS, data_per):
@@ -85,6 +98,11 @@ def dfs_concatanate(data_AS, data_per):
     return data
 
 def create_your_dataframe():
+
+    """
+    This function creates the user's requested dataframe
+    :return: The requested dataframe
+    """
     # Create an empty DataFrame object
     data = pd.DataFrame()
 
@@ -97,7 +115,13 @@ def create_your_dataframe():
     return data_con
 
 def create_bigraph_from_AS_relationships():
-    data = pd.read_csv('../../Datasets/AS-relationships/20210701.as-rel2.txt', sep="|", skiprows=180)
+
+    """
+    This function takes as input 20210701.as-rel2.txt  and creates a graph based on NetworkX library.
+    :return: A bipartite graph containing only peers nodes
+    """
+    path = '../../Datasets/AS-relationships/20210701.as-rel2.txt'
+    data = pd.read_csv(path, sep="|", skiprows=180)
     data.columns = ['node1', 'node2', 'link', 'protocol']
     data.drop(['protocol'], axis=1, inplace=True)
 
@@ -110,14 +134,17 @@ def create_bigraph_from_AS_relationships():
         # Check if node1 and node2 are peers
         if link == 0:
             graph.add_edge(node1, node2)
-    # print("=====================================")
-    # print("Stats about the data")
-    # print(nx.info(graph))
-    # print("=====================================")
+
     return graph
 
 def create_bigraph_from_netixlan():
-    data = json.load(open('../../Datasets/PeeringDB/netixlan.json'))
+
+    """
+    This function takes as input netixlan.json  and creates a graph based on NetworkX library.
+    :return: A bipartite graph ( with node1=ixlan_id and node2=asn)
+    """
+    path = '../../Datasets/PeeringDB/netixlan.json'
+    data = json.load(open(path))
     df = pd.DataFrame(data["data"])
     df.drop(['id', 'net_id', 'ix_id', 'name', 'notes', 'speed', 'ipaddr4', 'ipaddr6', 'is_rs_peer', 'operational', 'created', 'updated', 'status'], axis=1, inplace=True)
 
@@ -127,9 +154,5 @@ def create_bigraph_from_netixlan():
 
     for node1, node2 in df.values:
         graph.add_edge(node1, node2)
-    # print("=====================================")
-    # print("Stats about the data")
-    # print(nx.info(graph))
-    # print("=====================================")
 
     return graph
--- Analysis/aggregate_data/get_dataframe.py
+++ Analysis/aggregate_data/get_dataframe.py
@@ -1,15 +1,18 @@
-from data_aggregation_tools import create_your_dataframe
 import data_aggregation_tools as dat
 import networkx as nx
 
 if __name__ == "__main__":
-    data = create_your_dataframe()
+    data = dat.create_your_dataframe()
 
     # write dataframe to csv
     # with index False drop the first column where it enumarates the rows of the dataframe
     data.to_csv('final_dataframe.csv', index=False)
+
+    # create a graph based on AS-relationships
     G = dat.create_bigraph_from_AS_relationships()
     print(nx.info(G))
+
+    # create a graph based on netixlan
     H = dat.create_bigraph_from_netixlan()
     print(nx.info(H))
 
