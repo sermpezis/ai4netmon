5ea5977bc540f9e26013e00de3022b925ca7bb0b
xrhstosg <georgitsis1996@gmail.com>
2021-10-06 12:36:28 +0300

    Create a tool to generate node Embeddings

--- Analysis/graph_embeddings/example_script_generate_embeddings.py
+++ Analysis/graph_embeddings/example_script_generate_embeddings.py
@@ -1,10 +1,20 @@
 import networkx as nx
 import graph_embeddings_library as gel
-
+from Analysis.aggregate_data.data_aggregation_tools import create_bigraph_from_AS_relationships
 
 graph = nx.Graph()
-graph.add_nodes_from(['a','b','c'])
-method = 'example'
-dict_args = {'embedding_size':4, 'default_value':22}
-data = gel.generate_ASN_embeddings_from_graph(graph, method, **dict_args)
-print(data)
+method = 'node2vec'
+
+if method == 'example':
+    graph = nx.Graph()
+    graph.add_nodes_from(['a', 'b', 'c'])
+    method = 'example'
+    dict_args = {'embedding_size': 4, 'default_value': 22}
+    data = gel.generate_ASN_embeddings_from_graph(graph, method, **dict_args)
+elif method == 'node2vec':
+    graph = create_bigraph_from_AS_relationships()
+    print(nx.info(graph))
+    dict_args = {'dimensions': 64, 'walk_length': 10, 'num_walks': 80, 'workers': 1, 'window': 5, 'min_count': 1, 'batch_words': 4}
+    data = gel.generate_ASN_embeddings_from_graph(graph, method, **dict_args)
+else:
+    raise Exception('Not defined type of embeddings')
--- Analysis/graph_embeddings/graph_embeddings_library.py
+++ Analysis/graph_embeddings/graph_embeddings_library.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import os.path
+from node2vec import Node2Vec
 
-GRAPH_EMBEDDING_METHODS = ['example','node2vec']
+GRAPH_EMBEDDING_METHODS = ['example', 'node2vec']
+
 
 def my_example_embeddings_method(graph, embedding_size, default_value=1):
     """
@@ -10,11 +13,39 @@ def my_example_embeddings_method(graph, embedding_size, default_value=1):
     :return: (pandas dataframe object) a dataframe where each node of the <graph> has <embedding_size> embeddings all with the same <default_value>
     """
     index = graph.nodes()
-    columns = ['embedding_'+str(i) for i in range(embedding_size)]
+    columns = ['embedding_' + str(i) for i in range(embedding_size)]
     data = pd.DataFrame(default_value, index=index, columns=columns)
     return data
 
-# TODO: @Christos: if your are not familiar with **kwargs, check some online tutorial (e.g., https://www.geeksforgeeks.org/args-kwargs-python/, https://realpython.com/python-kwargs-and-args/)
+
+def node2vec(graph, dimensions, walk_length, num_walks, workers, window, min_count, batch_words):
+    """
+    :param graph: (networkx Graph object) a graph, based on which the method generates embeddings for its nodes
+    :param dimensions: (int) Embedding dimensions
+    :param walk_length: (int) Number of nodes in each walk
+    :param num_walks: (int) Number of walks per node
+    :param workers: (int) Number of workers for parallel execution
+    :param window: (int) This parameter should be =1 when we run our program in WINDOWS
+    :param min_count: (int)
+    :param batch_words: (int)
+    :return: A dataframe containing the embeddings that have been trained based on ASns
+    """
+    if not os.path.isfile('./embeddings.emb'):
+        EMBEDDING_FILENAME = './embeddings.emb'
+
+        node2vec_model = Node2Vec(graph, dimensions, walk_length, num_walks, workers)
+        model = node2vec_model.fit(window, min_count, batch_words)
+
+        # Look for most similar nodes. Output node names are always strings
+        model.wv.most_similar('2')
+        model.wv.save_word2vec_format(EMBEDDING_FILENAME)
+        data = pd.read_table("embeddings.emb", header=None, sep=" ")
+    else:
+        # Load model after Node2Vec.save
+        data = pd.read_table("embeddings.emb", header=None, sep=" ")
+    return data
+
+
 def generate_ASN_embeddings_from_graph(graph, method, **kwargs):
     """
     :param graph: (networkx Graph object) a graph, based on which the method generates embeddings for its nodes
@@ -25,10 +56,7 @@ def generate_ASN_embeddings_from_graph(graph, method, **kwargs):
     if method == 'example':
         data = my_example_embeddings_method(graph, **kwargs)
     elif method == 'node2vec':
-        # TODO: sth like the following: data = node2vec(graph, **kwargs)
-        # TODO: when you populate the code here, delete the following line with the "pass"
-        pass
+        data = node2vec(graph, **kwargs)
     else:
         raise Exception('Not defined method for generation of graph embeddings')
     return data
-
