f774f48404d3ced47b4e11a99de318ea8b6fa473
xrhstosg <georgitsis1996@gmail.com>
2021-10-07 15:01:20 +0300

    Create Similarity measures

--- Analysis/similarities/create_similarity_matrix.py
+++ Analysis/similarities/create_similarity_matrix.py
@@ -1,37 +1,71 @@
-'''
-TODO: if you don't know about environment variables, check e.g. https://www.twilio.com/blog/environment-variables-python
-
-I have added an example .env file in the parent folder
-You can edit it locally (i.e., put the correct path to file in your local pc) but do not commit/push it
-'''
-import os 
-# requires installing the following: pip3 install python-dotenv
+import os
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import json
 from dotenv import load_dotenv
 
-
 load_dotenv()
-ASN2ASN_FILENAME = os.getenv('ASN2ASN_FILENAME')
+key = 'ASN2ASN_FILENAME'
+ASN2ASN_FILENAME = os.getenv(key)
 
 
-### pseudocode for the calculation of the similariity matrix
+def load_dict_from(filename):
+    """
+    :param filename: The local path where the asn2asn.json file is stored
+    :return: a dictionary of the given .json file
+    """
+    with open(filename) as json_file:
+        data = json.load(json_file)
+    return data
+
+def similarity_function(data, data2):
+
+    metric = 'Gini'
+    inter = np.intersect1d(data, data2)
+    union = np.union1d(data, data2)
+    if metric == 'Gini':
+        similarity = len(inter)/len(union)
+    elif metric == 'Gini_strict':
+        inter_common_values = [x for x in inter if (data.index(x) & data2.index(x)) > 0]
+        similarity = len(inter_common_values)/len(union)
+    elif metric == 'cosine_similarity':
+        values1 = [x if x in union else 0 for x in data]
+        values2 = [x if x in union else 0 for x in data2]
+        if len(values1) > len(values2):
+            new_values2 = np.array([values2[i] if i < len(values2) else 0 for i in range(0, len(values1))])
+            new_similarity = cosine_similarity(np.array(values1).reshape(1, -1), new_values2.reshape(1, -1))
+            similarity = new_similarity[0][0]
+        else:
+            new_values1 = np.array([values1[i] if i < len(values1) else 0 for i in range(0, len(values2))])
+            new_similarity = cosine_similarity(new_values1.reshape(1, -1), np.array(values2).reshape(1, -1))
+            similarity = new_similarity[0][0]
+    else:
+        raise Exception('Not defined similarity method')
+    return similarity
+
+# with open('package.json') as json_file:
+#     data_dict = json.load(json_file)
+
+# calculation of the similarity matrix
 data_dict = load_dict_from(ASN2ASN_FILENAME)
-list_of_asns = list(data_dict.keys())
-for asn1, asn2 in all_pairs_of_asns_in_list_of_asns:
-	similarity(asn1, asn2) = similarity_function(data_dict[asn1],data_dict[asn2])
+list_containing_all_possible_pairs = []
 
+for asn in data_dict:
+    new_pairs = []
+    flag = True
+    for asn2 in data_dict[asn]:
+        if flag:
+            new_pairs.append(asn)
+            new_pairs.append(asn2)
+            flag = False
+        else:
+            new_pairs.append(asn2)
+    list_containing_all_possible_pairs.append(new_pairs)
 
-
-### pseudocode for the different similariity functions to be implemented
-#1 - metric: gini
-inter = intersection(data_dict[asn1].keys(), data_dict[asn2].keys())
-union = union(data_dict[asn1].keys(), data_dict[asn2].keys())
-similarity_function1 = len(inter)/len(union)
-
-#2 - metric: "gini-strict"
-inter_common_values = [x for x in inter if data_dict[asn1][x]==data_dict[asn1][x]]
-similarity_function2 = len(inter_common_values)/len(union)
-
-#3 - metric: cosine similarity
-values1 = [data_dict[asn1].get(x,0) for x in union]
-values2 = [data_dict[asn2].get(x,0) for x in union]
-similarity_function3 = cosine_similarity(values1, values2)
+similarity_dict = {}
+for i in range(0, len(list_containing_all_possible_pairs)-1):
+    asn1 = list_containing_all_possible_pairs[i]
+    for j in range(i+1, len(list_containing_all_possible_pairs)):
+        asn2 = list_containing_all_possible_pairs[j]
+        similarity_dict[asn1[0], asn2[0]] = similarity_function(asn1, asn2)
+    print(similarity_dict)
