d17264dc2dc1c54e6becc0b1941f27123575ef4c
xrhstosg <georgitsis1996@gmail.com>
2021-10-08 10:58:25 +0300

    Rename a function

--- Analysis/graph_embeddings/example_script_generate_embeddings.py
+++ Analysis/graph_embeddings/example_script_generate_embeddings.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import graph_embeddings_library as gel
-from Analysis.aggregate_data.data_aggregation_tools import create_bigraph_from_AS_relationships
+from Analysis.aggregate_data.data_aggregation_tools import create_graph_from_AS_relationships
 
 graph = nx.Graph()
 method = 'node2vec'
@@ -12,7 +12,7 @@ if method == 'example':
     dict_args = {'embedding_size': 4, 'default_value': 22}
     data = gel.generate_ASN_embeddings_from_graph(graph, method, **dict_args)
 elif method == 'node2vec':
-    graph = create_bigraph_from_AS_relationships()
+    graph = create_graph_from_AS_relationships()
     print(nx.info(graph))
     dict_args = {'dimensions': 64, 'walk_length': 10, 'num_walks': 80, 'workers': 1, 'window': 5, 'min_count': 1, 'batch_words': 4}
     data = gel.generate_ASN_embeddings_from_graph(graph, method, **dict_args)
