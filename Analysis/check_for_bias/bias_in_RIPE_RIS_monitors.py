dc39dd6c224ffb2b3a82bdca6505e478ebeaddb0
xrhstosg <georgitsis1996@gmail.com>
2021-10-12 18:03:55 +0300

    Fix plots

--- Analysis/check_for_bias/bias_in_RIPE_RIS_monitors.py
+++ Analysis/check_for_bias/bias_in_RIPE_RIS_monitors.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.distributions.empirical_distribution import ECDF
 
-PATH_RIPE_RIS_PEERS = '../Datasets/RIPE_RIS_peers_monitors/list_of_RIPE_RIS_peers.json'
+PATH_RIPE_RIS_PEERS = '../../Datasets/RIPE_RIS_peers_monitors/list_of_RIPE_RIS_peers.json'
 
 def read_ripe_peers():
 
@@ -18,42 +19,47 @@ def read_ripe_peers():
     return df
 
 def read_final_dataframe():
+    """
+    :return: A dataframe that is created from the concatenation of 3 datasets
+    """
 
-    return pd.read_csv('aggregate_data/final_dataframe.csv', sep=',')
-
+    return pd.read_csv('../aggregate_data/final_dataframe.csv', sep=',')
 
 def cdf_plot(ripe, final, feature):
-
-    x_final_sorted = np.linspace(0, 1, len(final))
-    x_ripe_sorted = np.linspace(0, 1, len(ripe))
-
-    fig = plt.figure()
-    plt.title('Feature: ' + str(feature), fontsize=14, pad=20)
-    ax1 = fig.add_subplot(121)
-    ax1.plot(x_final_sorted, final[feature])
+    """
+    :param ripe: Contains the AS numbers of RIPE RIS
+    :param final: Contains a dataframe combining 3 datasets
+    :param feature: Is the column name of final
+    """
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle('Feature: ' + str(feature), fontsize=14)
     ax1.set_xlabel('Final data')
     ax1.set_ylabel('CDF Analysis')
-
-    ax2 = fig.add_subplot(122)
-    merged_data = pd.merge(ripe, final, on=['ASn'], how='inner')
-    ax2.plot(x_ripe_sorted, merged_data[feature])
+    final_cdf = ECDF(final[feature])
+    ax1.plot(final_cdf.x, final_cdf.y)
+    merged_data = pd.merge(ripe, final, on='ASn', how='inner')
+    merged_data.sort_values('ASn', inplace=True)
+    merged_data.drop_duplicates(subset='ASn', keep=False, inplace=True)
+    merged_data.sort_values(feature, inplace=True)
     ax2.set_xlabel('Ripe data')
-    ax2.set_ylabel('CDF Analysis')
+    ripe_cdf = ECDF(merged_data[feature])
+    ax2.plot(ripe_cdf.x, ripe_cdf.y)
     plt.savefig(str(feature) + f'.png')
     plt.tight_layout()
     plt.show()
-    plt.clf()
-
 
 def histogram_plot(ripe, final, feature):
-
+    """
+    :param ripe: Contains the AS numbers of RIPE RIS
+    :param final: Contains a dataframe combining 3 datasets
+    :param feature: Is the column name of final
+    """
     plt.style.use('seaborn-deep')
     x = final[feature].astype(str)
     merged_data = pd.merge(ripe, final, on=['ASn'], how='inner')
     y = merged_data[feature].astype(str)
-    plt.hist(x, cumulative=True, density=1, bins=1000, histtype='step', label='final')
-    plt.hist(y, cumulative=True, density=1, bins=1000, histtype='step', label='ripe')
-    plt.legend(loc='upper right')
+    plt.hist([x, y], density=True, bins=10, histtype='bar', label=['final', 'ripe'], color=['blue', 'orange'])
+    plt.legend(prop={'size': 10})
     plt.suptitle('Feature: ' + str(feature), fontsize=14)
     plt.xlabel('Ripe data')
     plt.xticks(rotation='vertical')
@@ -62,23 +68,43 @@ def histogram_plot(ripe, final, feature):
     plt.tight_layout()
     plt.show()
 
+def test_CDF_plots(ripe, final, feature):
+    """
+    :param ripe: Contains the AS numbers of RIPE RIS
+    :param final: Contains a dataframe combining 3 datasets
+    :param feature: Is the column name of final
+    """
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle('Feature: ' + str(feature), fontsize=14)
+    print(final[feature])
+    my_cdf = ECDF(final[feature])
+    ax1.plot(my_cdf.x, my_cdf.y)
+
+    merged_data = pd.merge(ripe, final, on='ASn', how='inner')
+    merged_data.sort_values('ASn', inplace=True)
+    merged_data.drop_duplicates(subset='ASn', keep=False, inplace=True)
+    merged_data.sort_values(feature, inplace=True)
+    ripe_cdf = ECDF(merged_data[feature])
+    ax2.plot(ripe_cdf.x, ripe_cdf.y)
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == "__main__":
 
     ripe_df = read_ripe_peers()
     final_dataframe = read_final_dataframe()
     final_dataframe.rename(columns={'AS_rank_asn': 'ASn'}, inplace=True)
+    for column_name in final_dataframe.columns:
 
-for column_name in final_dataframe.columns:
-
-    dataTypeObj = final_dataframe.dtypes[column_name]
-
-    if dataTypeObj == np.int64 or dataTypeObj == np.float64:
-        ripe_sorted = ripe_df.sort_values(by=['ASn'], ascending=True)
-        final_sorted = final_dataframe.sort_values(by=[column_name], ascending=True)
-        cdf_plot(ripe_sorted, final_sorted, column_name)
-    elif dataTypeObj == np.object:
-        ripe_sorted = ripe_df.sort_values(by=['ASn'], ascending=True)
-        final_sorted = final_dataframe.sort_values(by=[column_name], ascending=True)
-        histogram_plot(ripe_sorted, final_sorted, column_name)
-
+        dataTypeObj = final_dataframe.dtypes[column_name]
+        if dataTypeObj == np.int64 or dataTypeObj == np.float64:
+            ripe_sorted = ripe_df.sort_values(by=['ASn'], ascending=True)
+            final_sorted = final_dataframe.sort_values(by=[column_name], ascending=True)
+            cdf_plot(ripe_sorted, final_sorted, column_name)
+            # test_CDF_plots(ripe_sorted, final_sorted, column_name)
+        elif dataTypeObj == np.object:
+            ripe_sorted = ripe_df.sort_values(by=['ASn'], ascending=True)
+            final_sorted = final_dataframe.sort_values(by=[column_name], ascending=True)
+            histogram_plot(ripe_sorted, final_sorted, column_name)
 
