10d1e00aec5d885e96d5444695210df557fcf453
xrhstosg <georgitsis1996@gmail.com>
2021-10-20 19:01:29 +0300

    Add hegemony_score to a csv file

--- Analysis/AS_hegemony/calculate_hegemony_for_each_AS.py
+++ Analysis/AS_hegemony/calculate_hegemony_for_each_AS.py
@@ -6,7 +6,7 @@ def read_json_file():
     """
     :return: A dataframe containing for each AS the AS_hegemony score for every other AS which connects
     """
-    f = open('package.json')
+    f = open('../../Datasets/AS_hegemony/merged_file.json')
     data = json.load(f)
     df = pd.DataFrame(data)
 
@@ -23,10 +23,11 @@ def remove_all_duplicates(df):
     # (example) if origin_Asn=1 and asn=19 exists 2 or more times in the dataset with different timestamp, we keep only the first appearance as the hege remains the same
     new_df = df.drop_duplicates(subset=['originasn', 'asn'], keep='first').reset_index(drop=True)
 
-    # to delete inverse column values (example origin_Asn=100 asn=70 and origin_Asn=70 and asn=100)
-    new_df['remove_inverse_duplicates'] = new_df.apply(lambda row: ''.join(sorted(str([row['originasn'], row['asn']]))),
-                                                       axis=1)
-    new_df = new_df.drop_duplicates('remove_inverse_duplicates')
+    # # to delete inverse column values (example origin_Asn=100 asn=70 and origin_Asn=70 and asn=100)
+    # new_df['remove_inverse_duplicates'] = new_df.apply(lambda row: ''.join(sorted(str([row['originasn'], row['asn']]))),
+    #                                                   axis=1)
+    # new_df = new_df.drop_duplicates('remove_inverse_duplicates')
+
     # keep only the columns that we need
     new_df = new_df[['originasn', 'asn', 'hege']]
 
@@ -38,4 +39,5 @@ cleaned_dataframe = remove_all_duplicates(df)
 
 # For the same asn sum all hegemony scores
 hege_df = cleaned_dataframe.groupby(['asn']).agg({'hege': 'sum'})
-print(hege_df)
+hege_df.reset_index(level=0, inplace=True)
+hege_df.to_csv('AS_hegemony.csv', index=False)
