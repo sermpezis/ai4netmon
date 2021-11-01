beb0b515d143f344d165b7d242994dad0025488d
xrhstosg <georgitsis1996@gmail.com>
2021-11-01 18:46:28 +0200

    Add RouteViews dataset and create deepcopy

--- Analysis/check_for_bias/get_analysis_plots.py
+++ Analysis/check_for_bias/get_analysis_plots.py
@@ -1,10 +1,7 @@
 import data_analysis_tool as dat
 
-
-ALL_DATASETS1 = ['Ripe_Atlas_probes']
-ALL_DATASETS2 = ['Ripe_Ris_monitors']
+ALL_DATASETS = ['Ripe_Ris_monitors', 'Ripe_Atlas_probes', 'RouteViews_peers']
 
 if __name__ == "__main__":
 
-    dat.plot_analysis(ALL_DATASETS1)
-    dat.plot_analysis(ALL_DATASETS2)
+    dat.plot_analysis(ALL_DATASETS)
