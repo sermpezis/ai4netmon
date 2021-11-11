749f10e3d6f0294f6e418224040dc6cb0939c5d6
xrhstosg <georgitsis1996@gmail.com>
2021-11-11 12:20:11 +0200

    An unsupervised machine learning extension library for producing graph embeddings

---
+++ Analysis/Karateclub_Library/call_library_method.py
@@ -0,0 +1,49 @@
+import networkx as nx
+import pandas as pd
+import numpy as np
+import os.path
+from karateclub import Diff2Vec
+
+if not os.path.isfile('./convert_to_consecutive.csv'):
+
+    PATH_AS_RELATIONSHIPS = '../../Datasets/AS-relationships/20210701.as-rel2.txt'
+    data = pd.read_csv(PATH_AS_RELATIONSHIPS, sep="|", skiprows=180, header=None)
+    data.columns = ['source', 'target', 'link', 'protocol']
+    data.drop(['link', 'protocol'], axis=1, inplace=True)
+
+    unique_nodes1 = set(data.source)
+    unique_nodes2 = set(data.target)
+
+    all_nodes = set(unique_nodes1.union(unique_nodes2))
+    sort_nodes = sorted(all_nodes)
+
+    # create consecutive numbers
+    temp_list = list(range(len(sort_nodes)))
+
+    # Create dict: Key is the real node and key is the consecutive node
+    dict = {k: v for k, v in zip(sort_nodes, temp_list)}
+
+    for key, value in dict.items():
+            data['source'][data['source'] == key] = value
+            data['target'][data['target'] == key] = value
+    data.to_csv("convert_to_consecutive.csv", sep=',', index=False)
+else:
+    data = pd.read_csv('convert_to_consecutive.csv', sep=",")
+
+
+graph = nx.Graph()
+graph.add_nodes_from(data.source, node_type="node")
+graph.add_nodes_from(data.target, node_type="node")
+
+for line in data.values:
+    graph.add_edge(line[0], line[1])
+
+dimensions = 64
+model = Diff2Vec(dimensions=dimensions)
+model.fit(graph)
+embedding = model.get_embedding()
+
+id = [i for i in range(0, len(embedding))]
+embedding = np.insert(embedding, 0, id, axis=1)
+pd.DataFrame(embedding).to_csv("Diff2Vec_embeddings.csv", sep=',', index=False)
+
