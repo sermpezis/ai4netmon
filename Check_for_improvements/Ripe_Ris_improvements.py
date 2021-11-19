eb7859a16cf226d589595bc06b16a472d400de7f
xrhstosg <georgitsis1996@gmail.com>
2021-11-19 11:35:40 +0200

    Refactor code

--- Check_for_improvements/Ripe_Ris_improvements.py
+++ Check_for_improvements/Ripe_Ris_improvements.py
@@ -2,7 +2,8 @@ import data_preprocessing as dp
 import call_ML_models as cmm
 import numpy as np
 
-karate_club_emb = ['Diff2Vec', 'NetMF', 'NodeSketch', 'Walklets', 'DeepWalk', 'Node2Vec_Local', 'Node2Vec_Global']
+karate_club_emb_64 = ['Diff2Vec', 'NetMF', 'NodeSketch', 'Walklets', 'Node2Vec_Local', 'Node2Vec_Global']
+karate_club_emb_128 = ['Diff2Vec', 'NetMF', 'NodeSketch', 'Walklets', 'DeepWalk']
 graph_emb_user_choice = ''
 graph_emb_dimensions = 128
 
@@ -10,7 +11,10 @@ improvement_df = dp.read_RIS_improvement_score()
 if graph_emb_user_choice == 'Node2Vec':
     embeddings_df = dp.read_Node2Vec_embeddings_file()
 else:
-    embeddings_df = dp.read_karateClub_embeddings_file(karate_club_emb[3], dimensions=graph_emb_dimensions)
+    if graph_emb_dimensions == 64:
+        embeddings_df = dp.read_karateClub_embeddings_file(karate_club_emb_64[3], dimensions=graph_emb_dimensions)
+    elif graph_emb_dimensions == 128:
+        embeddings_df = dp.read_karateClub_embeddings_file(karate_club_emb_128[3], dimensions=graph_emb_dimensions)
 mergedStuff = dp.merge_datasets(improvement_df, embeddings_df)
 
 y = mergedStuff['Improvement_score']
