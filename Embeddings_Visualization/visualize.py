55fbeefd7acc1b0b3de43aa464369baff812746d
xrhstosg <georgitsis1996@gmail.com>
2021-12-23 17:01:27 +0200

    Add visualization for RIPE RIS monitors

--- Embeddings_Visualization/visualize.py
+++ Embeddings_Visualization/visualize.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import numpy as np
 import matplotlib.patches as mpatches
 import umap.umap_ as umap
 import matplotlib.pyplot as plt
 import seaborn as sns
+import json
 
+RIPE_RIS_PEERS = '../Datasets/RIPE_RIS_peers/improvements_RIPE_RIS_peers_leave_one_out.json'
 STUB_ASES = '../Analysis/remove_Stubs_from_AS_relationships/Stub_ASes.csv'
+
 DEEPWALK_EMBEDDINGS_128 = 'StorePreprocessedEmb/PreprocessedDeepWalk128.csv'
 DIFF2VEC_EMBEDDINGS_128 = 'StorePreprocessedEmb/PreprocessedDiff2Vec128.csv'
 NETMF_EMBEDDINGS_128 = 'StorePreprocessedEmb/PreprocessedNetMF128.csv'
@@ -22,6 +26,9 @@ NODE2VEC_WL5_E3_GLOBAL = 'StorePreprocessedEmb/PreprocessedNode2Vec_wl5_e3_globa
 NODE2VEC_64_WL5_E1_GLOBAL = 'StorePreprocessedEmb/PreprocessedNode2Vec_wl5_global64.csv'
 BGP2VEC_64 = 'StorePreprocessedEmb/Preprocessedbgp2vec64.csv'
 BGP2VEC_32 = 'StorePreprocessedEmb/Preprocessedbgp2vec_32.csv'
+
+run_script_with_All_ASES = False
+choose_model = BGP2VEC_32
 
 
 def create_embedding_plot(two_dimensions, df_all, model_name):
@@ -87,11 +94,35 @@ def read_embedding_model(path):
     return new_data, models_name
 
 
-data, model_name = read_embedding_model(BGP2VEC_32)
-df_stubs = read_Stub_csv(STUB_ASES)
-# we have drop ASes that have no info about AS_rank_iso in create_csv_for_visualization.py,
-# that's why we have this difference (42 elements missing)
-df_all = data.merge(df_stubs, how='left', indicator=True).loc[lambda x: x['_merge'] == 'left_only']
-data_emb = df_all.drop(['ASN', '_merge', 'AS_rank_iso'], axis=1)
-umap_tranformed = call_umap(data_emb)
-create_embedding_plot(umap_tranformed, df_all, model_name)
+def read_RIS_improvement_score():
+    """
+    :return: A dataframe containing 2 columns. The first one contains ASNs and the second one the improvement score
+     that each ASN will bring to the Network
+    """
+    with open(RIPE_RIS_PEERS) as handle:
+        dictdump = json.loads(handle.read())
+
+    data = pd.DataFrame(dictdump.items(), columns=['ASN', 'Improvement_score'])
+
+    data['ASN'] = data['ASN'].astype(np.int64)
+    return data
+
+
+if run_script_with_All_ASES:
+    data, model_name = read_embedding_model(choose_model)
+    df_stubs = read_Stub_csv(STUB_ASES)
+    # we have drop ASes that have no info about AS_rank_iso in create_csv_for_visualization.py,
+    # that's why we have this difference (42 elements missing)
+    df_all = data.merge(df_stubs, how='left', indicator=True).loc[lambda x: x['_merge'] == 'left_only']
+    data_emb = df_all.drop(['ASN', '_merge', 'AS_rank_iso'], axis=1)
+    umap_tranformed = call_umap(data_emb)
+    create_embedding_plot(umap_tranformed, df_all, model_name)
+else:
+    data, model_name = read_embedding_model(choose_model)
+    ripe_monitors = read_RIS_improvement_score()
+    df_ripe = pd.merge(ripe_monitors, data, on=['ASN'], how='left')
+    # We need to drop one monitor because we do not have its embedding
+    df_ripe.dropna(inplace=True)
+    data_emb = df_ripe.drop(['ASN', 'AS_rank_iso', 'Improvement_score'], axis=1)
+    umap_tranformed = call_umap(data_emb)
+    create_embedding_plot(umap_tranformed, df_ripe, model_name)
