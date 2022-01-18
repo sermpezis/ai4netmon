4921b4cfb41fad5c6a41c1507a8f4878168a4949
xrhstosg <georgitsis1996@gmail.com>
2022-01-18 18:01:20 +0200

    Add plot

--- Analysis/Metric_not_counting_on_labels/metric_methodology.py
+++ Analysis/Metric_not_counting_on_labels/metric_methodology.py
@@ -6,8 +6,54 @@ from Analysis.Metric_Impact_Hijacking import metric_preprocessing as mp
 from Analysis.Metric_Impact_Hijacking import give_metric_ases_from_clusters as gmafc
 
 run_script_with_embeddings = True
-monitors_selected = 100
+monitors_selected = [10, 50, 100]
 FINAL_DATASET = '../aggregate_data/final_dataframe.csv'
+
+
+def calculate_metric_performance(df, number_of_mon):
+
+    df['estimated_impact'] = df.eq(1).sum(axis=1) / number_of_mon
+    df['square_error'] = pow((df['impact'] - df['estimated_impact']), 2)
+    rmse = np.sqrt(statistics.mean(df['square_error']))
+
+    return rmse
+
+
+def calculate_rmse_for_different_number_of_monitors(cluster_kmeans, data_CAIDA, data_caida_monitors, num_monitors):
+    final_list = []
+    for index, row in data_CAIDA.iterrows():
+        new_row = str(row['impact'])
+        monitors_counter = 0
+        rand_monitors = []
+        new_list = [new_row]
+
+        for monitor in cluster_kmeans:
+            if monitor in data_caida_monitors and monitors_counter < num_monitors:
+                monitors_counter = monitors_counter + 1
+                monitor_index = data_caida_monitors.index(monitor)
+                rand_monitor = row.iloc[monitor_index]
+                rand_monitors.append(rand_monitor)
+        new_list.append(rand_monitors)
+
+        head, tail = new_list
+        if new_list != [new_row]:
+            final_list.append([head] + tail)
+
+    # Create column names
+    initial_col = ['impact', 'observation_1']
+    obs_range = range(2, num_monitors + 1)
+    middle_cols = ['observation_' + str(i) for i in obs_range]
+    for i in middle_cols:
+        initial_col.append(i)
+
+    final_df = pd.DataFrame(final_list, columns=initial_col)
+    final_df[initial_col] = final_df[initial_col].apply(pd.to_numeric, errors='coerce', axis=1)
+    dcopy = final_df.copy(deep=True)
+
+    rmse = calculate_metric_performance(dcopy, num_monitors)
+
+    return rmse
+
 
 data_CAIDA = mp.read_caida_ases()
 ripe_monitors = mp.read_ripe_ris_monitors()
@@ -44,59 +90,20 @@ data_caida_monitors = list(data_CAIDA.columns.values)
 data_caida_monitors = data_caida_monitors[7: 295]
 data_caida_monitors = [int(x) for x in data_caida_monitors]
 
+rmse_list = []
+for number_of_monitors in monitors_selected:
+    rmse = calculate_rmse_for_different_number_of_monitors(cluster_kmeans, data_CAIDA, data_caida_monitors, number_of_monitors)
+    rmse_list.append(rmse)
+print(rmse_list)
 
-final_list = []
-for index, row in data_CAIDA.iterrows():
-    new_row = str(row['impact'])
-    monitors_counter = 0
-    rand_monitors = []
-    new_list = [new_row]
-
-    for monitor in cluster_spectral:
-        if monitor in data_caida_monitors and monitors_counter < monitors_selected:
-            monitors_counter = monitors_counter + 1
-            monitor_index = data_caida_monitors.index(monitor)
-            rand_monitor = row.iloc[monitor_index]
-            rand_monitors.append(rand_monitor)
-    new_list.append(rand_monitors)
-
-    head, tail = new_list
-    if new_list != [new_row]:
-        final_list.append([head] + tail)
-
-
-# Create column names
-initial_col = ['impact', 'observation_1']
-obs_range = range(2, monitors_selected+1)
-middle_cols = ['observation_' + str(i) for i in obs_range]
-counter = 1
-for i in middle_cols:
-    initial_col.append(i)
-
-final_df = pd.DataFrame(final_list, columns=initial_col)
-final_df[initial_col] = final_df[initial_col].apply(pd.to_numeric, errors='coerce', axis=1)
-
-for index, row in final_df.iterrows():
-    final_df['estimated_impact'] = final_df.eq(1).sum(axis=1)/monitors_selected
-for index, row in final_df.iterrows():
-    final_df['square_error'] = pow((final_df['impact'] - final_df['estimated_impact']), 2)
-
-for index, row in final_df.iterrows():
-    rmse = np.sqrt(statistics.mean(final_df['square_error']))
-print(rmse)
-
-
-# fontsize = 15
-# linewidth = 2
-# X = list(range(1, monitors_selected))
-# plt.plot(X, rmse, label="Min SpectralClustering k10", linewidth=linewidth)
-# plt.xscale('log')
-# plt.xlabel('#monitors', fontsize=fontsize)
-# plt.ylabel('RMSE', fontsize=fontsize)
-# plt.xticks(fontsize=fontsize)
-# plt.yticks(fontsize=fontsize)
-# plt.tight_layout()
-# plt.axis([1, 100, 0.1, 0.2])
-# plt.legend()
-# plt.grid(True)
-# plt.show()
+fontsize = 15
+linewidth = 2
+plt.plot(monitors_selected, rmse_list, linewidth=linewidth, marker="o")
+plt.xscale('log')
+plt.xlabel('#monitors', fontsize=fontsize)
+plt.ylabel('RMSE', fontsize=fontsize)
+plt.xticks(fontsize=fontsize)
+plt.yticks(fontsize=fontsize)
+plt.tight_layout()
+plt.grid(True)
+plt.show()
