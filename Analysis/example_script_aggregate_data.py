51dd2243d4190dab071a936ecfa229cc698314d4
sermpezis <sermpezis.pavlos@gmail.com>
2022-01-25 22:36:06 +0200

    Added updated data aggregation tools

--- Analysis/example_script_aggregate_data.py
+++ Analysis/example_script_aggregate_data.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
+# load existing file
 AGGREGATE_DATA_FNAME = '../data/aggregate_data/asn_aggregate_data_20211201.csv'
 
 df = pd.read_csv(AGGREGATE_DATA_FNAME, header=0, index_col=0)
+print(df)
+print(df.columns)
+
+
+# generate file
+from ai4netmon.Analysis.aggregate_data import data_aggregation_tools as dat
+
+ALL_DATASETS = ['AS_rank', 'personal', 'PeeringDB', 'AS_hegemony', 'Atlas_probes', 'RIPE_RIS', 'RouteViews']
+df = dat.create_dataframe_from_multiple_datasets(ALL_DATASETS)
+
 print(df)
 print(df.columns)
