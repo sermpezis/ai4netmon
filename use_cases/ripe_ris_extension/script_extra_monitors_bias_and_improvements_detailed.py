f21c20091ebca3224a6dccd3a54f004605e4d408
sermpezis <sermpezis.pavlos@gmail.com>
2022-03-02 18:18:53 +0200

    updated bias vs improvement detailed dataset

--- use_cases/ripe_ris_extension/script_extra_monitors_bias_and_improvements_detailed.py
+++ use_cases/ripe_ris_extension/script_extra_monitors_bias_and_improvements_detailed.py
@@ -8,6 +8,7 @@ AGGREGATE_DATA_FNAME = 'https://raw.githubusercontent.com/sermpezis/ai4netmon/de
 BIAS_DF = './data/bias_df__no_stubs.csv'
 IMPROVEMENTS = '../../data/misc/improvements20210601.txt'
 SAVE_CSV_FNAME = './data/df_bias_vs_improvement_detailed.csv'
+RENAME_COLUMNS = True
 
 
 ## load network data
@@ -38,17 +39,42 @@ df_bias = df_bias.add_prefix('bias_')
 
 # create dataframe from existing info for plotting
 df_plot = pd.merge(df_imp,df_bias, left_index=True, right_index=True)
-df_plot = pd.merge(df_plot,df['AS_rank_continent'], how='left', left_index=True, right_index=True)
+df_plot = pd.merge(df_plot,df[['AS_rank_continent','AS_rank_iso']], how='left', left_index=True, right_index=True)
 df_plot.drop(columns=['loc', 'IPv'], inplace=True)
 
 print(df_plot)
 
 
-colors_property = 'AS_rank_continent'
-unique_values = pd.unique(df_plot[colors_property]).tolist()
-dict_colors = {v:i for i,v in enumerate(unique_values)}
-df_plot['Colors'] = [dict_colors.get(v) for v in df_plot[colors_property]]
+# colors_property = 'AS_rank_continent'
+# unique_values = pd.unique(df_plot[colors_property]).tolist()
+# dict_colors = {v:i for i,v in enumerate(unique_values)}
+# df_plot['Colors'] = [dict_colors.get(v) for v in df_plot[colors_property]]
 df_plot['ASN'] = df_plot.index
+
+if RENAME_COLUMNS:
+    new_names = {
+        'bias_AS_rank_source': 'RIR region', 
+        'bias_AS_rank_iso': 'Country',
+        'bias_AS_rank_continent': 'Continent', 
+        'bias_AS_rank_numberAsns': 'Customer cone (#ASNs)',
+        'bias_AS_rank_numberPrefixes': 'Customer cone (#prefixes)', 
+        'bias_AS_rank_numberAddresses': 'Customer cone (#addresses)',
+        'bias_AS_hegemony': 'AS hegemony', 
+        'bias_AS_rank_total': '#neighbors (total)', 
+        'bias_AS_rank_peer': '#neighbors (peers)', 
+        'bias_AS_rank_customer': '#neighbors (customers)', 
+        'bias_AS_rank_provider': '#neighbors (providers)', 
+        'bias_peeringDB_ix_count': '#IXPs', 
+        'bias_peeringDB_fac_count': '#facilities', 
+        'bias_peeringDB_policy_general': 'Peering policy', 
+        'bias_peeringDB_info_type': 'Network type', 
+        'bias_peeringDB_info_ratio': 'Traffic ratio', 
+        'bias_peeringDB_info_traffic': 'Traffic volume', 
+        'bias_peeringDB_info_scope': 'Scope', 
+        'bias_is_personal_AS': 'Personal ASN'}
+    df_plot.rename(new_names, axis=1, inplace=True)
+print(df_plot.columns)
+
 df_plot.to_csv(SAVE_CSV_FNAME, index=False)
 
 print(df_plot)
