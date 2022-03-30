900d2786bd5a262171138730bf1781b2b1a09312
kostogls <38495639+kostogls@users.noreply.github.com>
2022-03-30 20:37:56 +0300

    Add peeringdb, AS_relationships and RouteViews

--- Analysis/aggregate_data/data_collectors.py
+++ Analysis/aggregate_data/data_collectors.py
@@ -1,6 +1,9 @@
 import requests
 from datetime import date, datetime 
 import time
+import os
+from urllib.request import urlopen
+import bz2
 from ihr.hegemony import Hegemony
 import pandas as pd
 import json
@@ -180,3 +183,108 @@ def AS_rank_collector(save_filename, window=500, offset=0):
 
     df = pd.DataFrame.from_dict(dict_, orient='index')
     df.to_csv(save_filename, index=False)
+
+    
+    
+def data_collectors_peeringdb(save_filename):
+    """
+    Collects data from CAIDA peering db and saves them to json file
+    :param save_filename: (str) the file to be save the dataset
+    """
+    today = date.today()
+    day = today.strftime("%d")
+    # subtract some days because usually the data of today are not up yet
+    d = today - timedelta(days=3)
+    d = str(d)
+    d = d[-2:]
+
+    d1 = today.strftime("%Y/%m")
+    d2 = today.strftime("%Y_%m_"+d)
+    d3 = today.strftime("%Y%m"+d)
+    PEERING_DB_URL = "https://publicdata.caida.org/datasets/peeringdb/{}/peeringdb_2_dump_{}.json"
+    data = requests.get(PEERING_DB_URL.format(d1, d2)).json()
+    with open(save_filename.format(d3), 'w') as f:
+        json.dump(data, f)
+   
+
+
+def as_rel_collector(save_filename):
+    """
+    Collects data from CAIDA peering db and saves them to json file
+    :param save_filename: (str) the file to be save the dataset
+    """
+    AS_REL_URL = "https://publicdata.caida.org/datasets/as-relationships/serial-2/{}.as-rel2.txt.bz2"
+    today = date.today()
+    d1 = today.strftime("%Y%m") + "01"
+
+    req = urlopen(AS_REL_URL.format(d1))
+    CHUNK = 16 * 1024
+
+    decompressor = bz2.BZ2Decompressor()
+    with open(save_filename.format(d1), 'wb') as fp:
+        while True:
+            chunk = req.read(CHUNK)
+            if not chunk:
+                break
+            fp.write(decompressor.decompress(chunk))
+    req.close()
+    
+    
+
+def remove_empty_lines(filename):
+    """
+    This function deletes the empty lines from the file
+    :param filename:
+    :return:
+    """
+    if not os.path.isfile(filename):
+        print("{} does not exist ".format(filename))
+        return
+    with open(filename) as filehandle:
+        lines = filehandle.readlines()
+
+    with open(filename, 'w') as filehandle:
+        lines = filter(lambda x: x.strip(), lines)
+        filehandle.writelines(lines)
+
+
+        
+def collect_route_views(save_filename):
+    """
+    Collects data from route views url and write them to a temporary text file. Then we clean the empty lines
+    and some rows that contain only bad words inside our data that we don't need. Finally we delete | from the data and
+    correct the spaces
+    :param save_filename: (str) the file to be save the dataset
+    :return: writes a txt file
+    """
+    ROUTE_VIEWS_URL = "http://www.routeviews.org/peers/peering-status.html"
+
+    today = date.today()
+    date_ = today.strftime("%Y%m%d")
+
+    data = requests.get(ROUTE_VIEWS_URL)
+    temp_file = 'temp.txt'
+    with open(temp_file, 'w') as temp:
+        temp.write(data.text)
+    remove_empty_lines(temp_file)
+
+    bad_words = ['ROUTEVIEWS COLLECTOR', '====================', '<pre>', '</pre>']
+
+    with open(temp_file) as oldfile, open(save_filename.format(date_), 'w') as newfile:
+        for line in oldfile:
+            if not any(bad_word in line for bad_word in bad_words):
+                newfile.write(line)
+
+    with open(save_filename.format(date_), 'r') as fin:
+        data = fin.read()
+        data = data.replace("|", " ")
+        data = data.splitlines(True)
+
+    for line in data:
+        print(' '.join(line.split()))
+
+    with open(save_filename.format(date_), 'w') as fout:
+        for line in data[5:]:
+            fout.write(' '.join(line.split()) + "\n")
+        
+
