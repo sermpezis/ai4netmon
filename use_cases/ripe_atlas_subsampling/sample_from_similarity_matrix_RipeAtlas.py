411fe2b1edbe6f869ea5a2678958d622223ddf6a
kostogls <38495639+kostogls@users.noreply.github.com>
2022-05-08 17:06:39 +0300

    Add map_function to map probes with asns

--- use_cases/ripe_atlas_subsampling/sample_from_similarity_matrix_RipeAtlas.py
+++ use_cases/ripe_atlas_subsampling/sample_from_similarity_matrix_RipeAtlas.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from Analysis.similarity import select_from_similarity as sfs
+from ai4netmon.Analysis.similarity import select_from_similarity as sfs
 import numpy as np
 # set total times of runs for the selection methods
 
@@ -73,6 +73,46 @@ def select_from_greedy_leastsimilar(data):
                                                                                       method='Greedy max',
                                                                                       nb_items=NB_ITEMS)
     pd.DataFrame(selected_from_greedy_least_similar).to_csv("selected_from_greedy_least_similar.csv")
+
+
+def map_function(selected_probes, dict_probes, sampling_method):
+    """
+    Function that maps the sampled probes dictionary (which are the ids in the data_of_ripe_atlas_probs text) to
+    their corresponding asns, for all ten sampled probes, and saves them to dataframe which is later written to csv
+    file.
+    Takes as input the dictionary of sampled probes, the dictionary of probes info loaded, and the sampling method
+    (kmeans or greedy least)
+    """
+    list_of_asn_dicts = []
+
+    for time in range(TOTAL_TIMES):
+        asn_dicts = []
+
+        for probe in selected_probes[str(time)]:
+            if probe is not None:
+                asn_dicts.append(list(filter(lambda dict_probes: dict_probes['id'] == int(probe), dict_probes)))
+
+        list_of_asn_dicts.append(asn_dicts)
+
+    final_asns = []
+
+    for time in range(TOTAL_TIMES):
+        asns_ = []
+
+        for i in range(len(list_of_asn_dicts[time])):
+            asns_.append([a_dict['asn_v4'] for a_dict in list_of_asn_dicts[time][i]])
+
+        final_asns.append(asns_)
+
+    for i in range(TOTAL_TIMES):
+        final_asns[i] = [item for sublist in final_asns[i] for item in sublist]
+
+    df_ = pd.DataFrame(final_asns)
+    df_ = df_.transpose()
+    df_.columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(df_)
+
+    pd.DataFrame(df_).to_csv("selected_from_{}_asns_of_probes.csv".format(sampling_method))
 
 
 
