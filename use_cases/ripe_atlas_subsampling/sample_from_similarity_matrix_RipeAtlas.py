ee3c3f66e3a22e3fb1d82089e88f987ce3e4bc53
kostogls <38495639+kostogls@users.noreply.github.com>
2022-05-24 17:23:03 +0300

    combine two select_from_kmeans functions to one

--- use_cases/ripe_atlas_subsampling/sample_from_similarity_matrix_RipeAtlas.py
+++ use_cases/ripe_atlas_subsampling/sample_from_similarity_matrix_RipeAtlas.py
@@ -18,40 +18,24 @@ df = df.fillna(0)
 NB_ITEMS = 1000
 
 
-def select_from_kmeans10(data):
+def select_from_kmeans(data, clusters):
     """
-    This function takes as input the dataframe of similarities, creates a dictionary that stores
-    the ASNs collected each time the k-means with 10 clusters is performed, in the form of (k,v), where the key
+    This function takes as input the dataframe of similarities and the number of clusters wanted, creates a dictionary that stores
+    the ASNs collected each time the k-means with selected clusters is performed, in the form of (k,v), where the key
     is the time the k-means algorithm runs, and value is a list of 1000 monitors that are collected.
     Then, the dictionary is stored to a csv in the form of dataframe.
     """
-    selected_from_k_means_10 = {}
+    selected_from_k_means = {}
     for time in range(TOTAL_TIMES):
-        selected_from_k_means_10[str(time)] = sfs.select_from_similarity_matrix(similarity_matrix=data, method='Clustering',
-                                                                            clustering_method='Kmeans', nb_clusters=10,
+        selected_from_k_means[str(time)] = sfs.select_from_similarity_matrix(similarity_matrix=data, method='Clustering',
+                                                                            clustering_method='Kmeans', nb_clusters=clusters,
                                                                             nb_items=NB_ITEMS)
-
-    pd.DataFrame(selected_from_k_means_10).to_csv("selected_from_k_means_10.csv")
-
-
-def select_from_kmeans20(data):
-    """
-    This function takes as input the dataframe of similarities, creates a dictionary that stores
-    the ASNs collected each time the k-means with 20 clusters is performed, in the form of (k,v), where the key
-    is the time the k-means algorithm runs, and value is a list of 500 monitors that are collected.
-    Then, the dictionary is stored to a csv in the form of dataframe.
-    """
-    selected_from_k_means_20 = {}
-    for time in range(TOTAL_TIMES):
-        selected_from_k_means_20[str(time)] = sfs.select_from_similarity_matrix(similarity_matrix=data, method='Clustering',
-                                                                            clustering_method='Kmeans', nb_clusters=20,
-                                                                            nb_items=NB_ITEMS)
-    for key, value in selected_from_k_means_20.items():
+    for key, value in selected_from_k_means.items():
         if len(value) < NB_ITEMS:
             while len(value) < NB_ITEMS:
                 value.append('NaN')
 
-    pd.DataFrame(selected_from_k_means_20).to_csv("selected_from_k_means_20.csv")
+    pd.DataFrame(selected_from_k_means).to_csv("selected_from_k_means_{}_mean.csv".format(clusters))
 
 
 def select_from_greedy_leastsimilar(data):
