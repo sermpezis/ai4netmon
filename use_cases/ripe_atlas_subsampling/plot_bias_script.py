b777d5d46b25049a7c9a130f810e3c12533b273b
kostogls <sofiakost@outlook.com>
2022-06-02 09:57:52 +0300

    update plot_bias_script.py

--- use_cases/ripe_atlas_subsampling/plot_bias_script.py
+++ use_cases/ripe_atlas_subsampling/plot_bias_script.py
@@ -30,6 +30,9 @@ elif SIMMATRIX == 'ASN':
     KMEANS_10 = './data/selected_from_k_means_10_mean.csv'
     KMEANS_20 = './data/selected_from_k_means_20_mean.csv'
     GREEDY_LEAST = './data/selected_from_greedy_least_similar_mean.csv'
+    SPECTRAL_10 = './data/selected_from_SpectralClustering_10_0.csv'
+    SPECTRAL_20 = './data/selected_from_SpectralClustering_20_0.csv'
+    SPECTRAL_100 = './data/selected_from_SpectralClustering_100_0.csv'
 
 # select features for visualization
 FEATURE_NAMES_DICT = bu.get_features_dict_for_visualizations()
@@ -40,30 +43,62 @@ df = dat.load_aggregated_dataframe(preprocess=True)
 
 ## load first list of selected asns with clustering and greedy methods.
 
-kmeans_10 = pd.read_csv(KMEANS_10)
-kmeans_20 = pd.read_csv(KMEANS_20)
-greedy_least = pd.read_csv(GREEDY_LEAST)
 
-# keep one column of sampled asns and drop nan values
+def process_df(name):
+    sampling_df = pd.read_csv(name)
+    sampling_dflist = sampling_df.iloc[:, 1]
+    sampling_dflist = sampling_dflist.dropna()
+    sampling_dflist = list(sampling_dflist)
+    return sampling_dflist
 
-kmeans_10list = kmeans_10.iloc[:, 1]
-kmeans_10list = kmeans_10list.dropna()
+#
+# kmeans_10 = pd.read_csv(KMEANS_10)
+# kmeans_20 = pd.read_csv(KMEANS_20)
+# greedy_least = pd.read_csv(GREEDY_LEAST)
+# spectral_10 = pd.read_csv(SPECTRAL_10)
+# spectral_20 = pd.read_csv(SPECTRAL_20)
+# spectral_100 = pd.read_csv(SPECTRAL_100)
+#
+# # keep one column of sampled asns and drop nan values
+#
+# kmeans_10list = kmeans_10.iloc[:, 1]
+# kmeans_10list = kmeans_10list.dropna()
+#
+# kmeans_20list = kmeans_20.iloc[:, 1]
+# kmeans_20list = kmeans_20list.dropna()
+#
+# greedy_least_list = greedy_least.iloc[:, 1]
+# greedy_least_list = greedy_least_list.dropna()
+#
+# spectral_10list = spectral_10.iloc[:, 1]
+# spectral_10list = spectral_10list.dropna()
+#
+# spectral_20list = spectral_20.iloc[:, 1]
+# spectral_20list = spectral_20list.dropna()
+#
+# spectral_100list = spectral_100.iloc[:, 1]
+# spectral_100list = spectral_100list.dropna()
+#
+# # turn pandas series into lists
+# kmeans_10list = list(kmeans_10list)
+# kmeans_20list = list(kmeans_20list)
+# greedy_least_list = list(greedy_least_list)
+# spectral_10list = list(spectral_10list)
+# spectral_20list = list(spectral_20list)
+# spectral_100list = list(spectral_100list)
 
-kmeans_20list = kmeans_20.iloc[:, 1]
-kmeans_20list = kmeans_20list.dropna()
 
-greedy_least_list = greedy_least.iloc[:, 1]
-greedy_least_list = greedy_least_list.dropna()
+# # put lists inside a list and iterate it to check whether there are indices in the lists of sampled asns
+# # that do not exist in the whole df
 
-# turn pandas series into lists
-kmeans_10list = list(kmeans_10list)
-kmeans_20list = list(kmeans_20list)
-greedy_least_list = list(greedy_least_list)
+kmeans_10list = process_df(KMEANS_10)
+kmeans_20list = process_df(KMEANS_20)
+greedy_least_list = process_df(GREEDY_LEAST)
+spectral_10list = process_df(SPECTRAL_10)
+spectral_20list = process_df(SPECTRAL_20)
+spectral_100list = process_df(SPECTRAL_100)
 
-# put lists inside a list and iterate it to check whether there are indices in the lists of sampled asns
-# that do not exist in the whole df
-
-sampled_lists = [kmeans_10list, kmeans_20list, greedy_least_list]
+sampled_lists = [kmeans_10list, kmeans_20list, greedy_least_list, spectral_10list, spectral_20list, spectral_100list]
 
 for list_ in sampled_lists:
     try:
@@ -83,6 +118,9 @@ network_sets_dict['RIPE Atlas'] = df.loc[(df['nb_atlas_probes_v4']>0) | (df['nb_
 network_sets_dict['KMEANS 10'] = df.loc[kmeans_10list]
 network_sets_dict['KMEANS 20'] = df.loc[kmeans_20list]
 network_sets_dict['GREEDY LEAST'] = df.loc[greedy_least_list]
+network_sets_dict['SPECTRAL 10'] = df.loc[spectral_10list]
+network_sets_dict['SPECTRAL 20'] = df.loc[spectral_20list]
+network_sets_dict['SPECTRAL 100'] = df.loc[spectral_100list]
 
 
 network_sets_dict_for_bias = {k:v[FEATURES] for k,v in network_sets_dict.items() if k != 'all'}
@@ -97,7 +135,7 @@ print_df.index = [n.replace('\n','') for n in FEATURE_NAMES_DICT.values()]
 print(print_df.round(2))
     
 # plot radar plot
-plot_df = bias_df[['RIPE Atlas', 'KMEANS 10', 'KMEANS 20', 'GREEDY LEAST']]
+plot_df = bias_df[['RIPE Atlas', 'KMEANS 10', 'KMEANS 20', 'GREEDY LEAST', 'SPECTRAL 10', 'SPECTRAL 20', 'SPECTRAL 100']]
 radar_chart.plot_radar_from_dataframe(plot_df, colors=None, frame='polygon', save_filename=FIG_RADAR_FNAME_FORMAT.format('all'), varlabels=FEATURE_NAMES_DICT)
 
 
@@ -106,5 +144,8 @@ network_sets_dict_plots = {'All ASes': network_sets_dict['all'],
                      'RIPE Atlas': network_sets_dict['RIPE Atlas'], 
                      'KMEANS 10': network_sets_dict['KMEANS 10'],
                      'KMEANS 20': network_sets_dict['KMEANS 20'],
-                           'GREEDY LEAST': network_sets_dict['GREEDY LEAST']}
+                           'GREEDY LEAST': network_sets_dict['GREEDY LEAST'],
+                           'SPECTRAL 10': network_sets_dict['SPECTRAL 10'],
+                           'SPECTRAL 20': network_sets_dict['SPECTRAL 20'],
+                           'SPECTRAL 100': network_sets_dict['SPECTRAL 100']}
 gdp.plot_all(network_sets_dict_plots, SAVE_PLOTS_DISTRIBUTION_FNAME_FORMAT, save_json=False, show_plot=True)
