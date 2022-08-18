b22323177551786860112165ff6918afb8df4ef9
sermpezis <sermpezis.pavlos@gmail.com>
2022-08-18 16:37:52 +0300

    added observable data for Atlas extension

--- use_cases/ripe_ris_extension/example_extra_Atlas_for_observable.py
+++ use_cases/ripe_ris_extension/example_extra_Atlas_for_observable.py
@@ -5,31 +5,13 @@ from ai4netmon.Analysis.aggregate_data import data_aggregation_tools as dat
 from ai4netmon.Analysis.bias import bias_utils as bu
 
 
-BIAS_DF = './data/bias_df__no_stubs_RIS.csv'
-BIAS_DF_GROUPS = './data/bias_df__no_stubs_RIS_groups.{}'
-IMPROVEMENT_DF = './data/df_bias_vs_improvement.csv'
-FIG_BIAS_SAVENAME = './figures/fig_cdf_bias_vs_nb_extra_monitors.png'
-MAX_EXTRA = 400
-
-FONTSIZE = 15
-LINEWIDTH = 2
-colors_infra = {'all':'k', 'RIS':'b', 'RV':'r', 'Atlas':'g', 'RIS & RV':'m'}
+BIAS_DF = './data/bias_df__no_stubs_Atlas.csv'
+BIAS_DF_GROUPS = './data/bias_df__no_stubs_Atlas_groups.{}'
 
 
 FEATURE_NAMES_DICT = bu.get_features_dict_for_visualizations() 
 FEATURES = list(FEATURE_NAMES_DICT.keys())
 FEATURE_NAMES_DICT_NO_NEWLINE = {k:v.replace('\n','') for k,v in FEATURE_NAMES_DICT.items()}
-
-
-
-'''
-Load improvement data
-'''
-impr_df = pd.read_csv(IMPROVEMENT_DF, header=0, index_col=0)
-impr_df = impr_df[['Y','ASN']]
-impr_df.set_index('ASN', inplace=True)
-impr_df.rename(columns={'Y':'Improvement proximity'}, inplace=True)
-
 
 '''
 Load AI4NetMon dataframe (with ASNs data)
@@ -40,7 +22,7 @@ df.index = df.index.astype('int')
 # select columns to include
 only_columns = ['AS_rank_source', 'AS_rank_numberAsns', 'AS_rank_numberPrefixes', 'AS_rank_iso', 'AS_rank_total', 
 	'peeringDB_info_scope', 'peeringDB_info_type', 'peeringDB_policy_general', 'peeringDB_ix_count', 'peeringDB_fac_count',
-	'is_in_peeringDB', 'nb_atlas_probes_v4', 'nb_atlas_probes_v6', 'is_routeviews_peer']
+	'is_in_peeringDB', 'is_ris_peer_v4', 'is_ris_peer_v6', 'is_routeviews_peer']
 df = df[only_columns] 
 
 # change column names
@@ -61,7 +43,7 @@ bias_df = pd.read_csv(BIAS_DF, header=0, index_col=0).transpose()
 bias_df.columns = [FEATURE_NAMES_DICT_NO_NEWLINE[i] for i in bias_df.columns]
 
 # transform absolute bias scores to relative scores in 100%
-bias_diff = (bias_df/bias_df.loc['RIPE RIS',:]-1)*100
+bias_diff = (bias_df/bias_df.loc['Atlas',:]-1)*100
 
 # group biases per group of dimensions (to reduce data size)
 FEATURE_GROUPS = {
@@ -76,7 +58,7 @@ for k,v in FEATURE_GROUPS.items():
 	new_bias_diff[k+' bias'] = bias_diff[v].mean(axis=1)
 
 # drop RIPE RIS, change index type to int, name index to ASN, round data, calculate total bias score
-new_bias_diff.drop('RIPE RIS',inplace=True)
+new_bias_diff.drop('Atlas',inplace=True)
 new_bias_diff.index = new_bias_diff.index.astype('float')
 new_bias_diff.index = new_bias_diff.index.astype('int')
 new_bias_diff.index.name = 'ASN'
@@ -88,5 +70,4 @@ new_bias_diff = new_bias_diff.round(2)
 Join bias dataframe with AI4NetMon dataframe
 '''
 new_bias_diff = new_bias_diff.join(df)
-new_bias_diff = new_bias_diff.join(impr_df)
 new_bias_diff.to_csv(BIAS_DF_GROUPS.format('csv'))
